72ae9d503290d71eea166f03854570888c341ec2
zakariaguediayeh-tech <zakariaguediayeh@gmail.com>
2026-07-14 11:51:43 +0200

    Delete ppl-github directory

--- ppl-github/sw.js
+++
@@ -1,3 +0,0 @@
-const C="ppl-v1";const A=["index.html","app.js","manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
